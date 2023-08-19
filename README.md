da534e302874fb068bbad37ea9a44f69e5391288
Owen <ocolegro@gmail.com>
2023-08-18 23:07:16 -0700

    fix run errors

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # automata-v0
+
+## Install
+
+```bash
+git submodule update --init --recursive
+
+poetry install
+```
+
+## Run
+
+``bash
+echo OPENAI_API_KEY_LOCAL=$OPENAI_API_KEY_LOCAL\\nLEETCODE_SESSION=$LEETCODE_SESSION > .env
+poetry run python automata_v0/run_vanilla_problem_solver.py
+```
--- automata_v0/utils.py
+++ automata_v0/utils.py
@@ -20,3 +20,51 @@ def load_existing_jsonl(file_path: str) -> list[dict]:
         with open(file_path, "r") as json_file:
             return [json.loads(line) for line in json_file]
     return []
+
+
+def extract_code(raw_response: str) -> str:
+    def _extract_unformatted(raw_response):
+        # Extract the class definition as before
+        class_definition_match = re.search(
+            r"class\s\S*:\s*.*?(?=\n\n|$)", raw_response, re.DOTALL
+        )
+        class_definition = (
+            class_definition_match[0] if class_definition_match else None
+        )
+
+        # Find the position of the class definition in the raw_response
+        class_position = (
+            class_definition_match.start() if class_definition_match else -1
+        )
+
+        # Extract the lines before the class definition
+        lines_before_class = raw_response[:class_position].strip().splitlines()
+
+        # Extract the import statements by filtering lines that start with 'import' or 'from'
+        import_statements = [
+            line
+            for line in lines_before_class
+            if line.startswith(("import", "from"))
+        ]
+
+        # Combine the import statements and the class definition
+        return "\n".join(import_statements + [class_definition])
+
+    if "```python" in raw_response:
+        cleaned_response = raw_response.split("```python")[1]
+        return cleaned_response.split("```")[0]
+    elif "```" in raw_response:
+        cleaned_response = raw_response.split("```")[1]
+        return cleaned_response.split("```")[0]
+    else:
+        return _extract_unformatted(raw_response)
+
+
+def prep_for_leetcode(code: str) -> str:
+    lines = code.split("\n")
+    modified_lines = ["class Solution():"]
+    for line in lines:
+        if line.startswith("def "):
+            line = "def " + line[4:].replace("(", "(self, ", 1)
+        modified_lines.append(f"  {line}")
+    return "\n".join(modified_lines)
